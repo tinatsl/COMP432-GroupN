3548149553434bda7e98796e5af3a76777b06a68
tinatsl <v_tsilin@live.concordia.ca>
2024-10-30 00:17:14 -0400

    added grid search

--- main.py
+++ main.py
@@ -30,6 +30,38 @@ X_test, y_test = get_data(test_loader)
 X = np.concatenate((X_train, X_test), axis=0)
 y = np.concatenate((y_train, y_test), axis=0)
 
-show_samples(X, y, 3)
+# show_samples(X, y, 3)
+
+# Instantiate model
+num_classes = 3
+resnet_18 = models.resnet18(weights=None)
+resnet_18.fc = nn.Linear(resnet_18.fc.in_features, num_classes)
+
+# Visualize model summary
+# summary(resnet_18, input_size=(3, 224, 224))
+
+net = NeuralNetClassifier(
+    resnet_18,      # Replace with model being tested
+    max_epochs=50,
+    lr=0.001,
+    criterion=nn.CrossEntropyLoss,
+    optimizer=optim.Adam,   # Already tried SGD and Adam variations
+    device='cuda' if torch.cuda.is_available() else 'cpu'
+    # callbacks=[EarlyStopping(patience=20)]  # high patience since data is complex
+)
+
+param_grid = {
+    'lr': [0.0001, 0.001, 0.01,],
+    'max_epochs': [25, 50],
+    'batch_size': [32, 64],
+    'optimizer': [optim.Adam]
+}
+
+grid_search = GridSearchCV(net, param_grid, refit=True, cv=3, scoring='accuracy', verbose=2)
+grid_search.fit(X, y)
+
+print("Best Parameters:", grid_search.best_params_)
+print("Best Score:", grid_search.best_score_)
+
 
 
