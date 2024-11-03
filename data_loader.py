f0244015cefad09e9b04856fbc37ebb388c96024
tinatsl <v_tsilin@live.concordia.ca>
2024-11-03 15:14:32 -0500

    added normalization

--- data_loader.py
+++ data_loader.py
@@ -4,57 +4,56 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 
 
-def get_loaders(data_path, test_ratio, img_size, batch_size=32):
-
-    """ This function generates train and test data loaders
+def get_loaders(data_path, img_size=224, batch_size=32):
+    """ This function generates train, validation, and test data loaders
 
     Parameters
     ----------
     data_path: str
-            path to target dataset
-    test_ratio: float
-            ratio for the test set with relation to the train set
+        path to target dataset
     img_size: int
-            specifies expected img size for the model
+        expected size of one data sample
     batch_size: int
-            batch size for data loader
+        batch size for data loader
 
     Returns
     -------
     train_loader: DataLoader
         loader for the train set
-
+    val_loader: DataLoader
+        loader for the validation set
     test_loader: DataLoader
-            loader for the test set
-
+        loader for the test set
     """
 
     transform = transforms.Compose([
         transforms.Resize((img_size, img_size)),
         transforms.ToTensor(),
+        transforms.Normalize(mean=[0.7626, 0.5242, 0.7113], std=[0.1492, 0.1949, 0.1401])
     ])
 
-    # get images as tensors
+    # Load the full dataset
     dataset = ImageFolder(data_path, transform=transform)
 
-    # define train and test ratio
-    train_size = int((1.0 - test_ratio) * len(dataset))
-    test_size = len(dataset) - train_size
+    # Calculate the sizes for train, validation, and test sets
+    train_size = int(0.7 * len(dataset))
+    valid_size = int(0.15 * len(dataset))
+    test_size = len(dataset) - train_size - valid_size
 
     generator = torch.Generator().manual_seed(10)
 
-    # split dataset using random_split
-    train_set, test_set = torch.utils.data.random_split(dataset, [train_size, test_size], generator=generator)
+    # Split dataset into train + validation and test
+    train_set, valid_set, test_set = torch.utils.data.random_split(dataset, [train_size, valid_size, test_size], generator=generator)
 
+    # Create data loaders
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
-    return train_loader, test_loader
-
+    return train_loader, val_loader, test_loader
 
 # Extract features and labels compatible with grid search
 def get_data(loader):
-
     X, y = zip(*[(data, labels) for data, labels in loader])
 
     # Concatenate along the first dimension
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
+from tensorflow.python.layers.core import dropout
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torchvision.models as models
@@ -15,6 +16,7 @@ from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 # Import functions
 from data_loader import get_loaders, get_data
+from model_train import train_model
 from samples import show_samples
 
 dataset_loc = './dataset_1/'
@@ -22,9 +24,10 @@ dataset_loc = './dataset_1/'
 
 torch.manual_seed(42)
 
-train_loader, test_loader = get_loaders(dataset_loc, 0.3, 224, 32)
-X_train, y_train = get_data(train_loader)
-X_test, y_test = get_data(test_loader)
+train_loader, valid_loader, test_loader = get_loaders(dataset_loc,224, 18)
+# X_train, y_train = get_data(train_loader)
+# X_valid, y_valid = get_data(valid_loader)
+# X_test, y_test = get_data(test_loader)
 
 # show_samples(X_train, y_train, 3)
 
@@ -36,31 +39,41 @@ resnet_18.fc = nn.Linear(resnet_18.fc.in_features, num_classes)
 # Visualize model summary
 # summary(resnet_18, input_size=(3, 224, 224))
 
-net = NeuralNetClassifier(
-    resnet_18,      # Replace with model being tested
-    max_epochs=50,
-    lr=0.001,
-    criterion=nn.CrossEntropyLoss,
-    optimizer=optim.Adam,   # Already tried SGD and Adam variations
-    device='cuda' if torch.cuda.is_available() else 'cpu'
-    # callbacks=[EarlyStopping(patience=20)]  # high patience since data is complex
-)
+# Train model
 
-param_grid = {
-    'lr': [0.0001, 0.001, 0.01],
-    'max_epochs': [25, 50],
-    'batch_size': [32, 64],
-    'optimizer': [optim.AdamW, optim.SGD],
-    'optimizer__weight_decay': [0, 0.01, 0.1],
-    'optimizer__momentum': [0.9, 0.95],
-    'optimizer__nesterov': [True, False]
-}
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(resnet_18.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0001)
+# optimizer = optim.Adam(resnet_18.parameters(), lr=0.0001)
 
-random_search = RandomizedSearchCV(net, param_grid, n_iter=10, refit=True, cv=3, scoring='accuracy', verbose=2)
-random_search.fit(X_train, y_train)
+train_model(resnet_18, train_loader, valid_loader, criterion, optimizer, max_epoch=50, device='cpu', patience=20) # 25 epochs seems better with normalized data
 
-print("Best Parameters:", random_search.best_params_)
-print("Best Score:", random_search.best_score_)
+
+# net = NeuralNetClassifier(
+#     resnet_18,      # Replace with model being tested
+#     max_epochs=50,
+#     lr=0.001,
+#     criterion=nn.CrossEntropyLoss,
+#     optimizer=optim.Adam,   # Already tried SGD and Adam variations
+#     device='cuda' if torch.cuda.is_available() else 'cpu'
+#     # callbacks=[EarlyStopping(patience=20)]  # high patience since data is complex
+# )
+#
+# param_grid = {
+#     'lr': [0.0001, 0.0005, 0.001],
+#     'max_epochs': [25, 30, 50],
+#     'batch_size': [32, 64],
+#     'optimizer': [optim.Adam, optim.SGD],
+#     'optimizer__weight_decay': [0.01, 0.1],
+#     'optimizer__momentum': [0.9, 0.95],
+#     'optimizer__nesterov': [True, False],
+#     'module__dropout': [0.2, 0.3]
+# }
+#
+# random_search = RandomizedSearchCV(net, param_grid, n_iter=10, refit=True, cv=3, scoring='accuracy', verbose=2)
+# random_search.fit(X_train, y_train)
+#
+# print("Best Parameters:", random_search.best_params_)
+# print("Best Score:", random_search.best_score_)
 
 
 
