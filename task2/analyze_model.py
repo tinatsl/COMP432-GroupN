96fb7a66bc0077096027aae90d205a2a054bcff2
tinatsl <v_tsilin@live.concordia.a>
2024-11-25 21:16:11 -0500

    update feature extraction

--- task2/analyze_model.py
+++ task2/analyze_model.py
@@ -2,30 +2,31 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-from sklearn.metrics import classification_report
+
 
 def save_analysis(model, data_loader, device, class_names, save_path):
     model.eval()
     features = []
-    predictions = []
     labels = []
 
     # Extract features
     with torch.no_grad():
+
+        # freeze encoder
+        model_encoder = torch.nn.Sequential(*(list(model.children())[:-1]))
+
         for images, label in data_loader:
             images = images.to(device)
             label = label.to(device)
-            outputs = model(images)
-
-            _, predicted = torch.max(outputs.data, 1)
+            outputs = model_encoder(images)
+            outputs = outputs.view(outputs.size(0), -1)
 
             features.append(outputs.cpu().numpy())
-            predictions.extend(predicted.cpu().numpy())
             labels.extend(label.cpu().numpy())
-    #combine and convert
+
+    # combine and convert
     features = np.vstack(features)  
     labels = np.array(labels)       
-    predictions = np.array(predictions) 
 
     # Calculate tsne
     tsne = TSNE(n_components=2, random_state=42)
@@ -51,7 +52,7 @@ def save_analysis(model, data_loader, device, class_names, save_path):
     plt.xlabel("t-SNE Dimension 1")
     plt.ylabel("t-SNE Dimension 2")
 
-    #Save outputs:
+    # save outputs:
     plt.savefig((save_path + '.png'), bbox_inches='tight')
     print('Saved tsne plot at:' + save_path + '.png')
 
@@ -59,9 +60,3 @@ def save_analysis(model, data_loader, device, class_names, save_path):
     np.save((save_path + '_features2D.npy'), features_2d)
     np.save((save_path +'_labels.npy'), labels)
     print('Saved features and labels')
-
-    report = classification_report(labels, predictions, target_names=class_names)
-    print(report)
-    with open((save_path + '_report.txt'), 'w') as file:
-        file.write(report)
-    
